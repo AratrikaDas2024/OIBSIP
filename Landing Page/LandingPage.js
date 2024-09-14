9ea0cf59c3591c27bc6f7c8be40877d1c7534502
AratrikaDas2024 <aratrikadas04@gmail.com>
2024-09-15 03:41:44 +0530

    Update LandingPage.js

--- Landing Page/LandingPage.js
+++ Landing Page/LandingPage.js
@@ -1,17 +1,4 @@
-// let my_image = document.querySelector('img');
 
-// my_image.onclick = function(){
-//     let img_src = my_image.getAttribute('src');
-//     print(img_src);
-//     if (img_src === "image.png")
-//     {
-//         my_image.setAttribute('src', "image1.png");
-//     }
-//     else
-//     {
-//         my_image.setAttribute('src', "image.png");
-//     }
-// }
 
 let my_image = document.getElementById('image');
 my_image.onclick = function () {
